f5b3c7904ddc0ddd35dec416c02e7812c8b603ba
taimoon <leongahman@gmail.com>
2025-05-15 21:40:33 +0800

    remove legacy

--- upgrade.sh
+++ upgrade.sh
@@ -21,31 +21,14 @@ function test_x64_chez {
     rm compiler-x86_64.so
 }
 
-function test_i686_legacy {
+function test_i686 {
     make rebuild-clean
-    # NOTE: vector-test fail is fine as legacy doesn't support (make-vector sz k)
-    echo "cross compiling i686 from legacy" && \
-    time make tests TARGET_ARCH=i686 SCM_CC="./compiler.out" -j
-    time make bootstrap-test TARGET_ARCH=i686 SCM_CC_BACKEND="compiler.scm" SCM_CC="./compiler.out" -j
-    mv compiler-2.out compiler-i686.out
     time make bootstrap-test TARGET_ARCH=i686 SCM_CC_BACKEND="compiler.scm" SCM_CC="./compiler-i686.out" -j
-    mv compiler-2.out compiler-i686.out
 }
 
-function test_x86_legacy {
+function test_x86 {
     make rebuild-clean
-    # NOTE: vector-test fail is fine as legacy doesn't support (make-vector sz k)
-    echo "cross compiling amd64 from legacy" && \
-    time \
-    make new-compiler \
-        TARGET_ARCH=i686 SCM_CC="./compiler.out" \
-        SCM_CC_BACKEND="compiler-x86_64.scm" \
-        SCM_NCC="./compiler-x86_64.out" -j &&\
     time make bootstrap-test TARGET_ARCH=x86-64 SCM_CC_BACKEND="compiler-x86_64.scm" SCM_CC="./compiler-x86_64.out" -j
-    mv compiler-2.out compiler-x86_64.out
-    time make bootstrap-test TARGET_ARCH=x86-64 SCM_CC_BACKEND="compiler-x86_64.scm" SCM_CC="./compiler-x86_64.out" -j
-    mv compiler-2.out compiler-x86_64.out
-    make rebuild-clean
 }
 
 function test_x64_i686_cross {
@@ -86,7 +69,7 @@ function test_x64_i686_cross {
 time scheme --script make-compiler-chez.scm &&\
 test_i686_chez &&\
 test_x64_chez &&\
-test_i686_legacy &&\
-test_x86_legacy &&\
+test_i686 &&\
+test_x86 &&\
 test_x64_i686_cross &&\
 exit 0
