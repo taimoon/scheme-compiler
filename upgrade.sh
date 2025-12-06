a20f136733f2c5608a953860b973dbc3c45cf5f5
taimoon <leongahman@gmail.com>
2025-12-06 17:52:26 +0800

    update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -xe
 export HEAP_SIZE=$((0xfffff8))
 export CHEZSCHEMELIBDIRS=".:./lib/"
 
@@ -10,40 +11,40 @@ export CHEZSCHEMELIBDIRS=".:./lib/"
 
 function test_i686_chez {
     make rebuild-clean
-    time make tests TARGET_ARCH=i686 SCM_CC="petite --script compiler.so" -j && \
-    time make bootstrap-test TARGET_ARCH=i686 SCM_CC_BACKEND="compiler.scm" SCM_CC="petite --script compiler.so" -j && \
+    time make tests TARGET_ARCH=i686 SCM_CC="petite --script compiler.so" -j
+    time make bootstrap-test TARGET_ARCH=i686 SCM_CC_BACKEND="compiler.scm" SCM_CC="petite --script compiler.so" -j
     rm compiler.so
 }
 
 function test_x64_chez {
     make rebuild-clean
-    time make tests TARGET_ARCH=x86-64 SCM_CC="petite --script compiler-x86_64.so" -j && \
-    time make bootstrap-test TARGET_ARCH=x86-64 SCM_CC_BACKEND="compiler-x86_64.scm" SCM_CC="petite --script compiler-x86_64.so" -j && \
+    time make tests TARGET_ARCH=x86-64 SCM_CC="petite --script compiler-x86_64.so" -j
+    time make bootstrap-test TARGET_ARCH=x86-64 SCM_CC_BACKEND="compiler-x86_64.scm" SCM_CC="petite --script compiler-x86_64.so" -j
     rm compiler-x86_64.so
 }
 
 function test_i686 {
     make rebuild-clean
-    time make bootstrap-test TARGET_ARCH=i686 SCM_CC_BACKEND="compiler.scm" SCM_CC="./compiler-i686.out" -j &&\
+    time make bootstrap-test TARGET_ARCH=i686 SCM_CC_BACKEND="compiler.scm" SCM_CC="./compiler-i686.out" -j
     mv compiler-2.out ./compiler-i686.out
 }
 
 function test_x86 {
     make rebuild-clean
-    time make bootstrap-test TARGET_ARCH=x86-64 SCM_CC_BACKEND="compiler-x86_64.scm" SCM_CC="./compiler-x86_64.out" -j &&\
+    time make bootstrap-test TARGET_ARCH=x86-64 SCM_CC_BACKEND="compiler-x86_64.scm" SCM_CC="./compiler-x86_64.out" -j
     mv compiler-2.out ./compiler-x86_64.out
 }
 
 function test_x64_i686_cross {
     make rebuild-clean
     ### testing boot x64 from i686
-    echo "cross compiling amd64 from i686" && \
+    echo "cross compiling amd64 from i686"
     time \
     make cross-compile-test \
         BUILD_ARCH=i686 BUILD_CC="./compiler-i686.out" \
         TARGET_ARCH=x86-64 SCM_CC_BACKEND="compiler-x86_64.scm" \
-        SCM_NCC="./compiler-x86_64-new.out" -j && \
-    echo "cross compiling i686 from amd64" && \
+        SCM_NCC="./compiler-x86_64-new.out" -j
+    echo "cross compiling i686 from amd64"
     time \
     make cross-compile-test \
         BUILD_ARCH=x86-64 BUILD_CC="./compiler-x86_64-new.out" \
@@ -53,13 +54,13 @@ function test_x64_i686_cross {
     
     make rebuild-clean
     ### testing boot i686 from x64
-    echo "cross compiling i686 from amd64" && \
+    echo "cross compiling i686 from amd64"
     time \
     make cross-compile-test \
         BUILD_ARCH=x86-64 BUILD_CC="./compiler-x86_64.out" \
         TARGET_ARCH=i686 SCM_CC_BACKEND="compiler.scm" \
-        SCM_NCC="./compiler-i686-new.out" -j && \
-    echo "cross compiling amd64 from i686" && \
+        SCM_NCC="./compiler-i686-new.out" -j
+    echo "cross compiling amd64 from i686"
     time \
     make cross-compile-test \
         BUILD_ARCH=i686 BUILD_CC="./compiler-i686-new.out" \
@@ -70,9 +71,9 @@ function test_x64_i686_cross {
 }
 
 time scheme --script make-compiler-chez.scm &&\
-test_i686_chez &&\
-test_x64_chez &&\
-test_i686 &&\
-test_x86 &&\
-test_x64_i686_cross &&\
+test_i686_chez
+test_x64_chez
+test_i686
+test_x86
+test_x64_i686_cross
 exit 0
